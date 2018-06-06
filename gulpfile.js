4f04a5a43f613a40f0a7ad96c79be108efca9a94
NicolasT <nicolas.temenides@smartangel.fr>
2018-06-06 17:39:17 +0200

    add postcss babel update deps, remove contact page

--- gulpfile.js
+++ gulpfile.js
@@ -1,66 +1,59 @@
 const gulp = require('gulp');
-const plugins = require('gulp-load-plugins')({
-  pattern: ['gulp-*', 'gulp.*'],
-  replaceString: /\bgulp[\-.]/
-});
-const autoprefixer = require('autoprefixer');
-const cssnext = require('cssnext');
-const browserSync = require('browser-sync');
+const g = require('gulp-load-plugins')({
+			pattern: ['gulp-*', 'gulp.*'],
+			replaceString: /\bgulp[\-.]/
+		});
+const reporter = require('postcss-reporter');
+const browser = require('postcss-browser-reporter');
+const cssnext = require('postcss-cssnext');
+const browserSync = require('browser-sync')
+const cssnano = require('cssnano');;
+const errorHandler = function (error) {console.log(error);this.emit('end');};
 
-gulp.task('browser-sync', () => {
-  browserSync({
-    proxy: '127.0.0.1:8080',
-    online: false
-  });
+gulp.task('browser-sync', function () {
+	browserSync({
+		proxy: "127.0.0.1:8080",
+		online: false
+	});
 });
 
 gulp.task('reload', () => {
   browserSync.reload();
 });
 
-gulp.task('images', () => {
-  gulp.src('public/images/**/*')
-    .pipe(plugins.cache(plugins.imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
-    .pipe(gulp.dest('public/images/'));
+gulp.task('images', function (){
+	return gulp.src('public/assets/**/*')
+		.pipe(g.plumber({errorHandler}))
+		.pipe(g.cache(g.imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+		.pipe(gulp.dest('public/assets/'));
 });
 
 gulp.task('styles', () => {
-  const postcssPlugins = [
-    autoprefixer({ browsers: ['last 1 version'] }),
-    cssnext
-  ];
-
-  gulp.src(['styles/main*.scss'])
-    .pipe(plugins.plumber({
-      errorHandler(error) {
-        console.log(error.message);
-        this.emit('end');
-      }
-    }))
-    .pipe(plugins.sass())
-    .pipe(plugins.postcss(postcssPlugins))
-    .pipe(gulp.dest('public/css/'))
-    .pipe(browserSync.reload({ stream: true }));
+  return gulp.src(['styles/main*.scss'])
+		.pipe(g.plumber({errorHandler}))
+		.pipe(g.sass())
+		.pipe(g.postcss([
+			cssnext({warnForDuplicates: false}), cssnano(), reporter(), browser(),
+		]))
+		.pipe(gulp.dest('public/css/'))
+		.pipe(browserSync.reload({stream:true}))
 });
 
-gulp.task('build', () => {
-
-});
-
-gulp.task('scripts', () => {
-  gulp.src(['scripts/*.js'])
-    .pipe(plugins.plumber({
-      errorHandler(error) {
-        console.log(error.message);
-        this.emit('end');
-      }
-    }))
+gulp.task('scripts', function (){
+	return gulp.src(['scripts/*.js'])
+		.pipe(g.plumber({errorHandler}))
+		.pipe(g.babel({
+			presets: ['env', 'minify']
+		}))
+		.pipe(g.uglify())
     .pipe(gulp.dest('public/js/'))
     .pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task('default', ['browser-sync'], () => {
-  gulp.watch('scripts/**/*.js', ['scripts']);
-  gulp.watch('styles/**/*.scss', ['styles']);
-  gulp.watch('views/**/*.pug', ['reload']);
-});
+gulp.task('build', gulp.parallel('scripts', 'styles', 'images'));
+
+gulp.task('default', gulp.series('browser-sync', function (){
+	gulp.watch("scripts/**/*.js", ['scripts']);
+	gulp.watch("styles/**/*.scss", ['styles']);
+	gulp.watch("views/**/*.pug", ['reload']);
+}));
